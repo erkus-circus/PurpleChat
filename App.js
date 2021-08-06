2b000c1a18b797f25608e289116c5bf0b23736b3
Eric Diskin <ericdiskin26@gmail.com>
2021-08-05 21:42:15 -0400

    Commtix

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const getNamesFromServer = () => {
+    return 
+}
+
+const Names = () => {
+    const [data, setData] = useState([])
+
+    if (data) {
+        fetch("https://lucid-detroit.com/friends.json").then((res) => res.json()).then((json) => { setData(json.names) }).catch((err)=>{alert(err)})
+    }
+    
+    return (
+        <FlatList 
+        data={data}
+        renderItem={(item) => (
+            <Text>Friends Name: {item.item}</Text>
+        )}></FlatList>
+    )
+}
+
+const App = () => {
+    
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <Names />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: "green",
+        marginTop: 20,
+    },
+    input: {
+        backgroundColor: "black",
+        color: "red",
+        borderRadius: 15,
+        textAlign: "center"
+    }
 });
+
+export default App;
